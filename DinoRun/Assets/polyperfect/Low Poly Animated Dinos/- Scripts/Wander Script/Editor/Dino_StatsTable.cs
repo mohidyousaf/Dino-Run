8175896e128c3093bda99b61ac411c082b208316
mohidyousaf <sheikhmohid23@gmail.com>
2021-09-04 18:38:33 +0500

    pushing assets

---
+++ DinoRun/Assets/polyperfect/Low Poly Animated Dinos/- Scripts/Wander Script/Editor/Dino_StatsTable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace PolyPerfect
+{
+    public class Dino_StatsTable : Common_StatsTable { }
+}
---
+++ DinoRun/Assets/polyperfect/Low Poly Animated Dinos/- Scripts/Wander Script/Editor/Dino_WanderScriptEditor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+namespace PolyPerfect
+{
+    [CustomEditor(typeof(Dino_WanderScript))]
+    [CanEditMultipleObjects]
+    public class Dino_WanderScriptEditor : Common_WanderScriptEditor { }
+    }
