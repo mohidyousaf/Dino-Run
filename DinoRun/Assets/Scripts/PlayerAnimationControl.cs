39fa648687956e4acbe4eda741be2cd8cfb35fd4
mohidyousaf <sheikhmohid23@gmail.com>
2021-09-10 01:49:15 +0500

    evolve feature added

--- DinoRun/Assets/Scripts/PlayerAnimationControl.cs
+++ DinoRun/Assets/Scripts/PlayerAnimationControl.cs
@@ -13,23 +13,33 @@ namespace PolyPerfect
         public bool attack = false;
         bool flag = false;
         bool NextState = false;
+        private int evolveCount=0;
+        private int eggCount=0;
         void OnTriggerEnter(Collider target)
         {
             if (target.gameObject.CompareTag("Egg"))
             {
                 Destroy(target.gameObject);
-                //while(target.gameObject)
-                //{
-                    
-                //}
-                NextState = true;
+
+                // increasing egg count by 1
+                eggCount++;
+                Debug.Log("egg count is " + eggCount);
+
+                //if egg count is multiple of 2 , evolve
+
+                if(eggCount % 2 ==0){
+                     Debug.Log("changing transition as egg count is multiple of 2");
+                    NextState = true;
+                }
+                   
                 
             }
         }
         //public Common_PlaySound soundPlayer;
         void Start()
         {
-            anim = GetComponent<Animator>();
+          
+            // anim = GetComponent<Animator>();
             if (anim != null)
             {
                 Debug.Log("Animator is not null");
@@ -40,9 +50,11 @@ namespace PolyPerfect
         void Update()
         {
             anim = GetComponent<Animator>();
-            Debug.Log("i am here");
+            Debug.Log("evolve count is"+ evolveCount);
             if (NextState)
             {
+                Debug.Log("evolve count is"+ evolveCount);
+
                 GameObject myTrex=(GameObject)Instantiate(Trex, transform.position, Quaternion.identity);
                 //gameObject.SetActive(false);
                 myTrex.SetActive(true);
