684d0bb3207337abe2e336a77c3fec0a52d0ced3
batool-ahmed <ba06180@st.habib.edu.pk>
2021-09-13 01:25:23 +0500

    working script

--- DinoRun/Assets/Scripts/WayPointController.cs
+++ DinoRun/Assets/Scripts/WayPointController.cs
@@ -6,43 +6,46 @@ using UnityEngine;
 public class WayPointController : MonoBehaviour
 {
     public Transform currentDino;
-    private List<Transform> waypoints;
+    //private List<Transform> waypoints;
+    public GameObject[] waypoints;
     private int wayPointIndex;
 
     void onEnable()
     {
-        waypoints = GetComponentsInChildren<Transform>().ToList();
-        waypoints.RemoveAt(0);
+        //waypoints = GetComponentsInChildren<Transform>().ToList();
+        //waypoints.RemoveAt(0);
         wayPointIndex = 0;
-        Debug.Log(waypoints.Count);
+        
         //currentDino = GameObject.FindWithTag("Dino").transform;
     }
 
     void Start()
     {
-        //currentDino = GameObject.FindWithTag("Dino").transform;
+        //Debug.Log("No. of objects are:", waypoints.Count);
+        currentDino = GameObject.FindWithTag("Dino").transform;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (!currentDino){
-        //    currentDino = GameObject.FindWithTag("Dino").transform;
-        //}
-
-        if (checkInWaypoint())
+        if (!currentDino){
+            currentDino = GameObject.FindWithTag("Dino").transform;
+        }
+        
+        if (wayPointIndex <= waypoints.Length-2)
         {
-            Debug.Log("WayPoint Arrived");
-            currentDino.rotation = waypoints[wayPointIndex].rotation;
-            if (wayPointIndex <= waypoints.Count-2)
+            if (checkInWaypoint())
             {
+                //Debug.Log("WayPoint Arrived");
+                //transform.Rotate()
+                currentDino.rotation = waypoints[wayPointIndex].transform.rotation;
                 wayPointIndex += 1;
             }
-            else 
-            {
-                reset();
-            }
+        }
+        else 
+        {
+            reset();
         }
     }
 
@@ -51,7 +54,7 @@ public class WayPointController : MonoBehaviour
         int dis = 10;
         //Transform currentWaypoint = waypoints[wayPointIndex];
         //if ( ( currentWaypoint - currentDino.position ).sqrMagnitude < presetDistance * presetDistance ) // sqrMagnitude, magnitude, Vector3.Distance, whatever your choice is
-        if (Vector3.Distance(waypoints[wayPointIndex].position, currentDino.position) <= dis) //insert norm formula here
+        if (Vector3.Distance(waypoints[wayPointIndex].transform.position, currentDino.position) <= dis) //insert norm formula here
         {
             return true;
         }
